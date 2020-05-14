2e309ee1787dfd2a4b49d679d17b899626f4e076
ByunKyungMin <bkm.change.min@gmail.com>
2020-05-14 11:14:07 +0900

    add class data

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
 import webbrowser
+from datetime import *
+import time
+import json
 
+weekday = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'] # define weekday
+today = weekday[datetime.today().weekday()] # day of week
+
+with open('private-subjects.json') as subjects_json: # open subject file
+    subjects = json.load(subjects_json)
+
+with open('timetable.json') as timetable_json: # open timetable file
+    timetable = json.load(timetable_json)
+
+while(True):
+
+
+def printTodaySubjects():
+    for day in range(1,7):
+    print(subjects[today][str(day)])
