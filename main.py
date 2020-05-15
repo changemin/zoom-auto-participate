77a12c145b74ab9e1ca8b1c5912469b45bef3847
ByunKyungMin <bkm.change.min@gmail.com>
2020-05-15 09:17:41 +0900

    remove timestamp

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import time
 import json
 
-with open('private-subjects.json', encoding='utf-8') as subjects_json: # open subject json file
+with open('subjects.json', encoding='utf-8') as subjects_json: # open subject json file
     subjects = json.load(subjects_json)
 
 with open('timetable.json', encoding='utf-8') as timetable_json: # open timetable json file
@@ -13,7 +13,7 @@ weekday = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'] # define weekday
 today = weekday[datetime.today().weekday()] # day of week
 
 def printTodaySubjects(): # print today't time table
-    print('『오늘의 시간표』')
+    print(f'『{today}day time table』')
     for classtime in range(1,timetable[today]["cnt"]+1):
         print(str(classtime) + "교시 : " + timetable[today][str(classtime)])
 
@@ -32,5 +32,6 @@ while(True):
             password = subjects[subject]["password"]
             url = meetingURL(meetingID, password)
             webbrowser.open(url)
-    print(f'현재시각 : {currnet_time}')
+            print(f'open subject : {subject}')
+    # print(f'현재시각 : {currnet_time}')
     time.sleep(10) # 10초마다 검사
