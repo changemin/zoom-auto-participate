272b880ad8c92242171a465ee22a2c0d8d9ebf6f
ByunKyungMin <bkm.change.min@gmail.com>
2020-08-28 09:08:37 +0900

    Fix 'Logic error'

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import webbrowser
+import threading
 from datetime import datetime
 import time
 import json
@@ -21,17 +22,23 @@ def meetingURL(meetingID, password): # return meeting link
     url = f'zoommtg://zoom.us/join?action=join&pwd={password}&confno={str(meetingID)}'
     return url
 
+def openMeeting(subject):
+    meetingID = subjects[subject]["meetingID"]
+    password = subjects[subject]["password"]
+    url = meetingURL(meetingID, password)
+    webbrowser.open(url)
+
 printTodaySubjects()
 
 while(True):
     currnet_time = datetime.now().strftime('%H:%M') # HH:MM
+    if(currnet_time == timetable["start-time"]["morning-assembly"]):
+        openMeeting("조회/종례")
     for classtime in range(1, timetable[today]["cnt"]+1):
         if(currnet_time == timetable["start-time"][str(classtime)]):
             subject = timetable[today][str(classtime)]
-            meetingID = subjects[subject]["meetingID"]
-            password = subjects[subject]["password"]
-            url = meetingURL(meetingID, password)
-            webbrowser.open(url)
+            openMeeting(subject)
             print(f'open subject : {subject}')
     # print(f'현재시각 : {currnet_time}')
     time.sleep(60) # 60초(= 1분)마다 검사
+
